9d1d6d532b910bc994b1e46963a86315fb7da7ee
enfu.g <guoenfu321@gmail.com>
2021-04-02 14:02:15 +0900

    add model preprocessing

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CODSUG2
 chat-oriented-dialogue-systems-using-gpt-2
+
+## Source
+
+- model : [gpt2-japanese](https://github.com/tanreinama/gpt2-japanese)
+- env : [nvidia-env](https://github.com/TachibanaET/My-Docker-Env-Templates)
+
+## Usage
+
+python model_preprocessing.py
---
+++ source/model_preprocessing.py
@@ -0,0 +1,54 @@
+# coding: UTF-8
+# developer : TachibanaET 
+import os
+import sys
+import urllib.request
+import tarfile
+import subprocess
+from subprocess import PIPE
+import tensorflow as tf
+from pprint import pprint
+
+import torch
+from torch import nn 
+
+class model_preprocessing:
+  def __init__(self):
+    self.gpt2_japanese_model_download_url = "https://www.nama.ne.jp/models/gpt2ja-medium.tar.bz2"
+    self.models_save_dir = "models/gpt2-pytorch-model-medium"
+    self.save_name = "gpt2ja-medium.tar.bz2"
+    self.model_path = os.path.join(self.models_save_dir, self.save_name)
+
+  def download_model(self):
+    urllib.request.urlretrieve(self.gpt2_japanese_model_download_url, self.model_path)
+    print('download_model ... done')
+
+  def unzip_model(self):
+    with tarfile.open(self.model_path) as tar:
+      tar.extractall(self.models_save_dir)
+
+    print('unzip_model ... done')
+
+  def convert_tf_to_torch(self):
+    tf_path = self.models_save_dir + '/' + f'gpt2ja-medium/model-10410000'
+    command = f'''
+      transformers-cli convert --model_type gpt2 \
+      --tf_checkpoint {tf_path} \
+      --pytorch_dump_output {self.models_save_dir} \
+      --config {self.models_save_dir}/config.json
+    '''
+
+    subprocess.run(command, shell=True, stdout=sys.stdout, stderr=sys.stderr)
+    print('convert_tf_to_torch ... done')
+
+  def clean_files(self):
+    subprocess.run(f'rm -rf {self.models_save_dir}/gpt2ja-medium', shell=True, stdout=sys.stdout, stderr=sys.stderr)
+    subprocess.run(f'rm -rf {self.models_save_dir}/{self.save_name}', shell=True, stdout=sys.stdout, stderr=sys.stderr)
+    print('clean files ... done')
+
+if __name__ == '__main__':
+  prep = model_preprocessing()
+  prep.download_model()
+  prep.unzip_model()
+  prep.convert_tf_to_torch()
+  prep.clean_files()
