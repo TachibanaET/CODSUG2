6aede3b7a633f5460abdad9c366c79a57d45bba4
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-21 00:08:30 +0000

    Adding tarfile member sanitization to extractall()

--- source/model_preprocessing.py
+++ source/model_preprocessing.py
@@ -25,7 +25,26 @@ class model_preprocessing:
 
   def unzip_model(self):
     with tarfile.open(self.model_path) as tar:
-      tar.extractall(self.models_save_dir)
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
+          
+      
+      safe_extract(tar, self.models_save_dir)
 
     print('unzip_model ... done')
 
